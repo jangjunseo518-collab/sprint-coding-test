df27351c1acd6a353bc5f15d9663041de0814516
HamJiwon <skxo0039@gmail.com>
2026-05-20 10:28:11 +0900

    Fix: jiwon-12925 파일 수정

--- programmers/week1/12925/함지원.java
+++
@@ -1,9 +0,0 @@
-import java.util.stream.*;
-
-public class Solution {
-    public int solution(int n) {
-        return Stream.of(String.valueOf(n).split(""))
-                .mapToInt(Integer::parseInt)
-                .sum();
-    }
-}
