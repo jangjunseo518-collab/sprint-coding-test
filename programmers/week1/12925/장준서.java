950783e527a14d9bc9a4496c6776e8b242aaba45
애니프사희망 <jangjunseo518@gmail.com>
2026-05-26 14:51:26 +0900

    Add: junseo-12925 (#28)
    Co-authored-by: jangjunseo518-collab <wkdwnstj00@naver.com>

---
+++ programmers/week1/12925/장준서.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        //Integer.parseInt()는 문자열을 숫자로 바꿔주는 java내장 메서드
+        // 문자열에 숫자가 들어있을때는 정상 작동 문자열에 문자가 들어있으면 오류.
+        return answer;
+    }
+}
