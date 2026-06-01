4313dc1734838340d07d1a613984cdd49f6c146b
HamJiWon <129733725+HamJiWeon@users.noreply.github.com>
2026-06-01 13:18:24 +0900

    Add: jiwon-12947 (#46)

---
+++ programmers/week2/12947/함지원.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+
+        char[] arr = String.valueOf(x).toCharArray();
+
+        int sum = 0;
+        for (char c : arr) {
+            sum += c - '0';
+        }
+
+        return x % sum == 0;
+    }
+}
