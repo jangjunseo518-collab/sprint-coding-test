24a617afc55263ba3a0e517700fda4ae29cc56dd
강다연 <142641618+K-Dayeon03@users.noreply.github.com>
2026-06-01 09:31:40 +0900

    [2주차] dayeon-12947 (#44)
    * Add : 문자열을 정수로 바꾸기-12925
    
    * Add : 약수의 합 구하기
    
    * Refactor : IntStream add
    
    Refactor solution method to use IntStream for summing divisors.
    
    * Rename : dayeon-12928
    
    * Add : dayeon - 12931
    
    * Add : dayeon - 12932
    
    * Add : dayeon - 12933
    
    * Add : dayeon-12944
    
    * Add : dayeon-12944
    
    * Add: dayeon-12947
    
    * Add: dayeon-12954

---
+++ programmers/week2/12947/강다연.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        String str = Integer.toString(x);
+        int sum = 0;
+        for(int i =0; i<str.length(); i++){
+            sum += str.charAt(i) - '0';
+        }
+
+        return x%sum == 0 ? answer : false;
+
+
+
+    }
+}
---
+++ programmers/week2/12954/강다연.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long[] solution(int x, int n) {
+        // 2*(0+1) = 2
+        // 2*(1+1) = 4
+        // 2*(2+1) = 6
+        // 2*(3+1) = 8
+        // 2*(4+1) = 10
+        long[] answer = {};
+        answer = new long[n];
+        for(int i=0; i<n; i++){
+            answer[i] = (long) x* (i+1);
+        }
+        return answer;
+
+    }
+}
