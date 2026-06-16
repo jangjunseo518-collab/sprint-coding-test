8df81758dfd1d0ba7de7281eed8ee1227a2b21c3
HamJiwon <skxo0039@gmail.com>
2026-06-16 09:16:19 +0900

    Add: jiwon-12951

---
+++ programmers/week5/12951/함지원.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {
+        String[] arr = s.split("");
+
+        arr[0] = arr[0].toUpperCase();
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1].equals(" ")) {
+                arr[i] = arr[i].toUpperCase();
+            } else {
+                arr[i] = arr[i].toLowerCase();
+            }
+        }
+
+        return String.join("", arr);
+    }
+}
